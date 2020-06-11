4cc9cfbbf79ff40dc55c35727493eb6c459d354e
ivine369 <ivinekemuma014@gmail.com>
2020-06-11 15:07:32 +0300

    add goals

--- Goals/src/app/app.module.ts
+++ Goals/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
